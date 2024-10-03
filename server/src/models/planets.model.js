67b0fcdbc8920d0dbb12594f78fa44c95a9e9017
sagaradesara <sagaradesara1420@gmail.com>
2024-10-03 19:46:23 +0530

    Load planets data in tests

--- server/src/models/planets.model.js
+++ server/src/models/planets.model.js
@@ -5,17 +5,17 @@ const { parse } = require("csv-parse");
 
 const planets = require("./planets.mongo");
 
+function isHabitablePlanet(planet) {
+  return (
+    planet["koi_disposition"] === "CONFIRMED" &&
+    planet["koi_insol"] > 0.36 &&
+    planet["koi_insol"] < 1.11 &&
+    planet["koi_prad"] < 1.6
+  );
+}
+
 function loadPlanetsData() {
   return new Promise((resolve, reject) => {
-    function isHabitablePlanet(planet) {
-      return (
-        planet["koi_disposition"] === "CONFIRMED" &&
-        planet["koi_insol"] > 0.36 &&
-        planet["koi_insol"] < 1.11 &&
-        planet["koi_prad"] < 1.6
-      );
-    }
-
     fs.createReadStream(
       path.join(__dirname, "..", "..", "data", "kepler_data.csv")
     )
